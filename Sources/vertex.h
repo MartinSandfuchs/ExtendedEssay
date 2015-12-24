7c5e7ef5eaeb307dd5524406a7eec41f4f180bf5
killerberry <martin@cluginbuehl.ch>
2015-12-24 15:15:47 +0100

    Create vertex.h

---
+++ Sources/vertex.h
@@ -0,0 +1,20 @@
+#ifndef VERTEX_H
+#define VERTEX_H
+
+#include "vector3d.h"
+#include "vector2d.h"
+
+struct Vertex
+{
+    Vector3f* p  = 0;
+    //QPointF texCoord;
+    Vector2f texCoord;
+};
+
+struct Triangle{
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+};
+
+#endif // VERTEX_H
