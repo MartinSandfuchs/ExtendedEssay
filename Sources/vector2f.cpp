d4c7ac32060c6c7d863ba21c6613e4df5c729d9f
killerberry <martin@cluginbuehl.ch>
2015-12-24 15:12:50 +0100

    Create vector2f.cpp

---
+++ Sources/vector2f.cpp
@@ -0,0 +1,31 @@
+#include "vector2d.h"
+
+Vector2f::Vector2f(const float &x, const float &y) :
+    x(x), y(y) {
+}
+
+Vector2f& Vector2f::operator=(const Vector2f &other) {
+    this->x = other.x;
+    this->y = other.y;
+    return *this;
+}
+
+//Operators
+Vector2f Vector2f::operator+(const Vector2f &vec) const {
+    return Vector2f(this->x + vec.x, this->y + vec.y);
+}
+
+Vector2f Vector2f::operator-(const Vector2f &vec) const {
+    return Vector2f(x - vec.x, y - vec.y);
+}
+
+Vector2f Vector2f::operator*(const float &val) const {
+    return Vector2f(x * val, y * val);
+}
+
+bool Vector2f::operator!=(const Vector2f &vec) const {
+    return x != vec.x || y != vec.y;
+}
+bool Vector2f::operator==(const Vector2f &vec) const {
+    return x == vec.x && y == vec.y;
+}
