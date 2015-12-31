bb91c59a94c0212c7df5ca49f75340a19d41e701
Martin Sandfuchs <martin@cluginbuehl.ch>
2015-12-31 18:22:55 +0100

    Update widget.h

--- Sources/widget.h
+++ Sources/widget.h
@@ -59,16 +59,16 @@ private:
     string path = "../Resources";
 
 private slots:
-    void paintEvent(QPaintEvent *);
+    void paintEvent(QPaintEvent *) override;
 
-    void mousePressEvent(QMouseEvent *e);
-    void mouseReleaseEvent(QMouseEvent *e);
-    void mouseMoveEvent(QMouseEvent *e);
-    void wheelEvent(QWheelEvent *e);
+    void mousePressEvent(QMouseEvent *e) override;
+    void mouseReleaseEvent(QMouseEvent *e) override;
+    void mouseMoveEvent(QMouseEvent *e) override;
+    void wheelEvent(QWheelEvent *e) override;
 
-    void keyPressEvent(QKeyEvent *e);
-    void keyReleaseEvent(QKeyEvent *e);
-    void resizeEvent(QResizeEvent *);
+    void keyPressEvent(QKeyEvent *e) override;
+    void keyReleaseEvent(QKeyEvent *e) override;
+    void resizeEvent(QResizeEvent *) override;
 
     void updateScene();
 
